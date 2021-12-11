04b84866b622ffb51b46f02a25b53a2fcacdf592
Antoni Gzara <antek.gzara@gmail.com>
2021-12-11 02:07:51 +0100

    uploaded files

---
+++ code/YoungLeokaldia.exe/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace YoungLeokaldia.exe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.wyjdźToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.animation1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wyjdźToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(134, 36);
+            // 
+            // wyjdźToolStripMenuItem
+            // 
+            this.wyjdźToolStripMenuItem.Name = "wyjdźToolStripMenuItem";
+            this.wyjdźToolStripMenuItem.Size = new System.Drawing.Size(133, 32);
+            this.wyjdźToolStripMenuItem.Text = "Wyjdź";
+            this.wyjdźToolStripMenuItem.Click += new System.EventHandler(this.wyjdźToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::YoungLeokaldia.exe.Properties.Resources.leokaldia_przycięta;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(579, 456);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // animation1
+            // 
+            this.animation1.Interval = 1;
+            this.animation1.Tick += new System.EventHandler(this.animation1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(579, 456);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YOUNG_LEOKALDIA.EXE";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem wyjdźToolStripMenuItem;
+        private System.Windows.Forms.Timer animation1;
+    }
+}
+
---
+++ code/YoungLeokaldia.exe/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+/*
+ * kod który tu stworzyłem jest bardzo chaotyczny ponieważ próbowałem poruszać oknem jak najbardziej chaotycznie się da
+ * chyba się udało w miarę możliwości
+ * 
+ */
+
+namespace YoungLeokaldia.exe
+{
+    public partial class Form1 : Form
+    {
+
+        private SoundPlayer SzklankiPlayer = new SoundPlayer(Properties.Resources.Young_Leosia___Szklanki);
+
+        private int AnimationFrameCount = 0;
+        private bool AnimationTypeX = false;
+        private bool AnimationTypeY = false;
+        private int AnimationChangeNumber = 0;
+        private int Speed = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void wyjdźToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SzklankiPlayer.PlayLooping();
+            animation1.Start();
+            AnimationChangeNumber = 10 * (new Random().Next(1, 3));
+        }
+
+        private void animation1_Tick(object sender, EventArgs e)
+        {
+            AnimationFrameCount++;
+
+            if (AnimationTypeX)
+            {
+                Location = new Point(this.Location.X + Speed, this.Location.Y); // przesuń leokaldię w prawo
+            }
+            else
+            {
+                Location = new Point(this.Location.X - Speed, this.Location.Y) ; //przesuń leokaldię w lewo
+            }
+
+            if (AnimationTypeY)
+            {
+                Location = new Point(this.Location.X, this.Location.Y + Speed); // przesuń leokaldię w górę
+            }
+            else
+            {
+                Location = new Point(this.Location.X, this.Location.Y - Speed); // przesuń leokaldię w dół
+            }
+
+            if (AnimationFrameCount > AnimationChangeNumber)
+            {
+                //if (AnimationTypeX) { AnimationTypeX = false; } else { AnimationTypeX = true; }
+                //AnimationTypeX = Convert.ToBoolean(new Random().Next(0, 2)); // nie najlepsze rozwiązanie, ale działą
+                
+                //AnimationTypeY = Convert.ToBoolean(new Random().Next(0, 2)); // nie najlepsze rozwiązanie ale przyjamniej działa XD
+                AnimationFrameCount = 0;
+                AnimationChangeNumber = 10 * (new Random().Next(1, 3)); //losowanie odległości do przejścia czy coś takiego
+
+                //próby aby kierunki były jak najbardziej randomowe 
+
+                /*
+                if (new Random().Next(0, 1) == 0)
+                {
+                    if (AnimationTypeY) { AnimationTypeY = false; } else { AnimationTypeY = true; }
+                }
+                if (new Random().Next(5, 8) == 6 || new Random().Next(5, 8) == 5)
+                {
+                    if (AnimationTypeX) { AnimationTypeX = false; } else { AnimationTypeX = true; }
+                }
+                */
+
+                //to wydaje się bardziej randomowe
+                try // gdyby milisekunda winosiła 0, program będzie chciał ją pozielić 
+                {
+                    if (DateTime.Now.Millisecond % DateTime.Now.Millisecond == 0)
+                    {
+                        if (AnimationTypeY) { AnimationTypeY = false; } else { AnimationTypeY = true; }
+                    }
+
+                    if (DateTime.Now.Millisecond % DateTime.Now.Millisecond + new Random().Next(0, 10) == 0)
+                    {
+                        if (AnimationTypeX) { AnimationTypeX = false; } else { AnimationTypeX = true; }
+                    }
+                }
+                catch
+                {
+                    if (DateTime.Now.Millisecond % DateTime.Now.Millisecond + 1 == 0)
+                    {
+                        if (AnimationTypeY) { AnimationTypeY = false; } else { AnimationTypeY = true; }
+                    }
+
+                    if (DateTime.Now.Millisecond % DateTime.Now.Millisecond + new Random().Next(0, 10) == 0)
+                    {
+                        if (AnimationTypeX) { AnimationTypeX = false; } else { AnimationTypeX = true; }
+                    }
+                }
+                /* //to nie działa, nie wiem dlaczego, jest godzina 01:11 i nie chce mi się nawet nad tym myśleć więc lepiej to olać. Ta cześć kody miała sprawdzać czy okno nie wyszło poza ekran i przejść na środek ekranu jeśli się poza nim znajdowało czy coś
+                if(Location.X > Screen.PrimaryScreen.WorkingArea.Width)
+                {
+                    Location = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2, Location.Y);
+                }
+                if (Location.X < Screen.PrimaryScreen.WorkingArea.Width)
+                {
+                    Location = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2, Location.Y);
+                }
+                if(Location.Y > Screen.PrimaryScreen.WorkingArea.Height)
+                {
+                    Location = new Point(Location.X, Screen.PrimaryScreen.WorkingArea.Height / 2);
+                }
+                if (Location.Y < Screen.PrimaryScreen.WorkingArea.Height)
+                {
+                    Location = new Point(Location.X, Screen.PrimaryScreen.WorkingArea.Height / 2);
+                }
+                */
+
+                if (!IsOnScreen(this))
+                {
+                    CenterToScreen();
+                }
+
+            }
+
+            //Console.WriteLine(AnimationFrameCount.ToString() + " " + AnimationTypeX.ToString() + " " + AnimationTypeY.ToString()); //tylko do debugowania!!!
+        }
+
+        //kod ze stack overflow: https://stackoverflow.com/questions/987018/determining-if-a-form-is-completely-off-screen
+        public bool IsOnScreen(Form form)
+        {
+            Screen[] screens = Screen.AllScreens;
+            foreach (Screen screen in screens)
+            {
+                Rectangle formRectangle = new Rectangle(form.Left, form.Top,
+                                                         form.Width, form.Height);
+
+                if (screen.WorkingArea.Contains(formRectangle))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
